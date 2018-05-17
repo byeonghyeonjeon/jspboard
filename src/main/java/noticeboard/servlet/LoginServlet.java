f3c97c9244f5f1ddf160c8878708b1fb572c736e
qudgus1447 <qudgus1447@gmail.com>
2018-05-17 11:53:26 +0900

    websocket 제거

--- src/main/java/noticeboard/servlet/LoginServlet.java
+++ src/main/java/noticeboard/servlet/LoginServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
 import noticeboard.model.BoardVO;
 import noticeboard.service.NoticeBoardService;
